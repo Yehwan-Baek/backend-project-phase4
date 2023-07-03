976b844f1a34ab8e599e090b5d761d2ddeeb8351
Yehwan Baek <baekokok98@gmail.com>
2023-07-03 20:38:55 +1000

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
   resources :anime_genres, only: [:create]
-  resources :characters
-  resources :release_dates
-  resources :genres
-  resources :watch_lists
-  resources :reviews
+  resources :characters, only: [:index, :show, :create]
+  resources :release_dates, only: [:index, :show]
+  resources :genres, only: [:index, :show]
+  resources :watch_lists, only: [:create]
+  resources :reviews, only: [:index, :show]
   resources :animes do
     resoureces :characters
+    resources :reviews
   end
-  resources :users
+
+  resources :users do
+    resources :reviews
+    resources :watch_lists
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
